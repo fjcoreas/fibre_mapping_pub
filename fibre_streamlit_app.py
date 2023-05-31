7949f65bfdd256186544f14590ef88bdfa87bc46
Francisco Coreas <fjcoreas@gmail.com>
2023-05-31 16:53:42 -0600

    sd

--- fibre_streamlit_app.py
+++ fibre_streamlit_app.py
@@ -399,8 +399,8 @@ def folium_map(df, selection):
                                        style=("background-color: white; color: #333333; font-family: arial; font-size: 12px; padding: 10px;"))) # setting style for popup box
         
         fttb2_line.geojson.add_child(
-        folium.features.GeoJsonTooltip(fields=['description', 'Name'],
-                                       aliases=['Description: ', 'Name: '],
+        folium.features.GeoJsonTooltip(fields=['Field_1', 'Type', 'Country', 'Comments', 'SHAPE', 'LABEL', 'Name', 'Eqpt_Label'],
+                                       aliases=['Field_1: ', 'Type: ', 'Country: ', 'Comments: ', 'SHAPE: ', 'LABEL: ', 'Name: ', 'Eqpt_Label: '],
                                        style=("background-color: white; color: #333333; font-family: arial; font-size: 12px; padding: 10px;"))) # setting style for popup box
 
 
